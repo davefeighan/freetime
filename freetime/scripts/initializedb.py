45abb880bb03d8df901d7bc93dce7570905e8371
Neil Basu <nbasu02@gmail.com>
2014-02-01 17:51:18 -0800

    Initialize freetime database and remove of MyModel

--- freetime/scripts/initializedb.py
+++ freetime/scripts/initializedb.py
@@ -11,27 +11,9 @@ from pyramid.paster import (
 
 from ..models import (
     DBSession,
-    MyModel,
     Base,
     )
 
 
-def usage(argv):
-    cmd = os.path.basename(argv[0])
-    print('usage: %s <config_uri>\n'
-          '(example: "%s development.ini")' % (cmd, cmd))
-    sys.exit(1)
-
-
 def main(argv=sys.argv):
-    if len(argv) != 2:
-        usage(argv)
-    config_uri = argv[1]
-    setup_logging(config_uri)
-    settings = get_appsettings(config_uri)
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    Base.metadata.create_all(engine)
-    with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+    os.system('createdb freetime -U postgres')
